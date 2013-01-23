ab120d90cab2812c2220fc9ed2fceb02b3c567ef
Aaron Parecki <aaron@parecki.com>
2013-01-23 13:44:06 -0800

    Updates community page, adds a search box to search the oauth-wg mailing list archives

--- community/index.php
+++ community/index.php
@@ -27,17 +27,20 @@ require('../includes/_header.php');
 
 			<p>The OAuth community is dedicated improving the spec and library codebase, spreading awareness of the protocol and documenting and showing, through screencasts, wireframes, mockups and other illustrative designs, how best to put OAuth into use.</p>
 
-			<p>Please visit the <a href="http://wiki.oauth.net">community wiki</a> for information on <a href="http://wiki.oauth.net/TestCases ">test cases</a>, <a href="http://wiki.oauth.net/ContributingToOAuth">contributing</a> and more.</p>
-
 			<h3 id="mailing-lists">Mailing Lists</h3>
 			
-			<p>The <a href="http://groups.google.com/group/oauth/">OAuth Core Mailing List</a> is where you can ask questions, get help, and get to know the rest of the OAuth community.</p>
+			<p>The <a href="http://groups.google.com/group/oauth/">OAuth Google Group</a> is where you can ask questions, get help, and get to know the rest of the OAuth community. You can <a href="http://groups.google.com/group/oauth/subscribe">subscribe</a> and <a href="http://groups.google.com/group/oauth/topics?hl=en">view the archives</a>.</p>
 			
-			<p>Membership on the list is currently <strong>open</strong> and you can <a href="http://groups.google.com/group/oauth/subscribe">subscribe</a>. The <a href="http://groups.google.com/group/oauth/topics?hl=en">archives</a> are also open and are available for review.</p>
-			
-			<p><a href="http://wiki.oauth.net/MailingLists">Additional OAuth Mailing Lists</a> can be found on the wiki (for example, language-specific lists).</p>
-			
-			<p>There is also an <a href="https://www.ietf.org/mailman/listinfo/oauth">OAuth mailing list</a> setup for work related to <span class="caps">IETF</span> standardization.</p>
+			<p>The <a href="https://www.ietf.org/mailman/listinfo/oauth">IETF OAuth mailing list</a> is for work related to <span class="caps">IETF</span> standardization.</p>
+
+      <p>
+        <form action="http://www.google.com/search" method="get">
+          Search the Archives: <input type="text" class="search-query" name="q" placeholder="Search ietf.org/oauth">
+          <input type="submit" value="Search" />
+          <input type="hidden" name="as_sitesearch" value="http://www.ietf.org/mail-archive/web/oauth/">
+          <input type="hidden" name="tbs" value="sbd:1,cdr:1,cd_min:1/1/1999">
+        </form>
+      </p>
 
 			<?php include('../includes/_edit_banner.php'); ?>
 
