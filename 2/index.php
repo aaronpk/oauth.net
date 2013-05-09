aa2c455f383911e762d1e0ef369939b8978a3553
Michal Ochman <ocherek@gmail.com>
2013-05-09 17:40:17 +0200

    Add Citrix ShareFile to the list of services that support OAuth 2

--- 2/index.php
+++ 2/index.php
@@ -93,6 +93,7 @@ require('../includes/_header.php');
 				<li><a href="https://developers.google.com/accounts/docs/OAuth2">Google</a></li>
 				<li><a href="http://www.meetup.com/meetup_api/auth/#oauth2">Meetup</a></li>
         <li><a href="http://www.salesforce.com/us/developer/docs/api_rest/Content/quickstart_oauth.htm">Salesforce</a></li>
+        <li><a href="http://www.sharefile.com/">Citrix ShareFile</a></li>
 				<li><a href="http://developers.soundcloud.com/docs/api/reference">SoundCloud</a></li>
 				<li><a href="https://do.com">Do.com (draft 22)</a></li>
 				<li><a href="http://msdn.microsoft.com/en-us/library/live/hh243647.aspx">Windows Live</a></li>
