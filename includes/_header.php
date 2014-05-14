94d0785644c8387a4b50feb4ff65c5ef2ed0396c
Aaron Parecki <aaron@parecki.com>
2014-05-14 09:14:01 -0700

    update pingback/webmention endpoints

--- includes/_header.php
+++ includes/_header.php
@@ -1,5 +1,7 @@
 <?php
-@header('X-Pingback: http://pingback.me/aaronpk/xmlrpc');
+@header('X-Pingback: http://webmention.io/aaronpk/xmlrpc');
+@header('Link: <https://webmention.io/aaronpk/webmention>; rel="webmention"');
+
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
