4533716d19469c4900587f81f7146dccf7225b16
Aaron Parecki <aaron@parecki.com>
2014-05-14 08:45:50 -0700

    add "Covert Redirect" advisory

--- advisories/index.php
+++ advisories/index.php
@@ -20,7 +20,6 @@ require('../includes/_header.php');
       <div id="secondary" class="column span-18 append-1 prepend-1 first last">
         <ul class="navigation">
           <li><a class="selected" href="/advisories">Security Advisories</a></li>
-          <li><a href="/advisories/2009-1">2009.1</a></li>
         </ul>
       </div>
     </div>
@@ -29,14 +28,18 @@ require('../includes/_header.php');
       <h2>Security Advisories</h2>
 			<p>The OAuth community is committed to identifying and addressing any security issues raised relating to the OAuth protocol and extensions. Any identified threat will be published on this page as soon as it is safe to do so. Due to the nature of many security threats, they cannot be disclosed before sufficient notice is given to vulnerable parties.</p>
 
-			<h3>Known Threats</h3>
-
 			<p>The following are known security threats and the protocol version they affect:</p>
+
+      <h3>OAuth 2.0</h3>
+      <ul>
+        <li><a href="/advisories/2014-1-covert-redirect/">2014.1 Covert Redirect </a></li>
+      </ul>
+
 
 			<h3>OAuth Core 1.0</h3>
 
 			<ul>
-				<li><a href="/advisories/2009-1">2009.1 Session Fixation Attack</a></li>
+				<li><a href="/advisories/2009-1/">2009.1 Session Fixation Attack</a></li>
 			</ul>
 
 			<h3>How to Report Security Threats</h3>
