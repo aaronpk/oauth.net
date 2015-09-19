92268736ba68d70b0acb1438055f2be5f99d45e4
Aaron Parecki <aaron@parecki.com>
2015-09-18 21:02:00 -0700

    fix typo

--- books/index.php
+++ books/index.php
@@ -31,7 +31,7 @@ require('../includes/_header.php');
 	  <a href="https://www.manning.com/books/oauth-2-in-action"><img src="oauth_in_action.jpg" alt="OAuth in Action Cover" title="OAuth in Action" style="float:left; max-width: 25%; margin: 10px;"></a>
 			<p><a href="https://www.manning.com/books/oauth-2-in-action"><i>OAuth 2 in Action</i></a> by Justin Richer and Antonio Sanso covers the OAuth 2 protocol in depth, providing hands-on exercises for building clients, authorization servers, and protected resources in various configurations. The book covers the structure and components of an OAuth 2 system, common vunlerabilities and mitigations, and several protocols that are built on and around OAuth 2.</p>
 			
-			<p>This book is currently available for <a href="https://www.manning.com/books/oauth-2-in-action">early-access pre-order from the publisher</a>. Pre-ordering the book gives you access to portions of the book in electronic format as it's being completed by the authors. You'll also be able to provide feadback during the production process.</p>
+			<p>This book is currently available for <a href="https://www.manning.com/books/oauth-2-in-action">early-access pre-order from the publisher</a>. Pre-ordering the book gives you access to portions of the book in electronic format as it's being completed by the authors. You'll also be able to provide feedback during the production process.</p>
 	  </div>
     </div>
 
