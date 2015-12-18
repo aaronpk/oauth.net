fa9d7df5373530579675d4c7e83cda71be8bd2a0
Aaron Parecki <aaron@parecki.com>
2015-12-18 08:16:31 -0800

    fix markup, add book cover image

--- books/index.php
+++ books/index.php
@@ -32,10 +32,9 @@ require('../includes/_header.php');
         <p><a href="https://www.manning.com/books/oauth-2-in-action"><i>OAuth 2 in Action</i></a> by Justin Richer and Antonio Sanso covers the OAuth 2 protocol in depth, providing hands-on exercises for building clients, authorization servers, and protected resources in various configurations. The book covers the structure and components of an OAuth 2 system, common vunlerabilities and mitigations, and several protocols that are built on and around OAuth 2.</p>        
         <p>This book is currently available for <a href="https://www.manning.com/books/oauth-2-in-action">early-access pre-order from the publisher</a>. Pre-ordering the book gives you access to portions of the book in electronic format as it's being completed by the authors. You'll also be able to provide feedback during the production process.</p>
       </div>
-    </div>
-    
-    <div>
-        <a href="https://www.packtpub.com/application-development/mastering-oauth-2/?utm_source=GC-oauth.net&utm_medium=pod&utm_campaign=1784395407"><img src="mastering_oauth_2.0_cover.jpg" alt="Mastering Oauth 2.0 Cover" title="Mastering OAuth 2.0" style="float:left; max-width: 25%; margin: 10px; border: 1px #ccc solid;"></a>
+	
+      <div>
+        <a href="https://www.packtpub.com/application-development/mastering-oauth-2/?utm_source=GC-oauth.net&utm_medium=pod&utm_campaign=1784395407"><img src="mastering_oauth_2.jpg" alt="Mastering Oauth 2.0 Cover" title="Mastering OAuth 2.0" style="float:left; max-width: 25%; margin: 10px; border: 1px #ccc solid;"></a>
         <p><a href="https://www.packtpub.com/application-development/mastering-oauth-2/?utm_source=GC-oauth.net&utm_medium=pod&utm_campaign=1784395407"><i>Mastering OAuth 2.0</i></a> by Charles Bihis takes a detailed and hands-on approach to explaining the protocol, highlighting important pieces of information along the way.</p>
       </div>
     </div>
