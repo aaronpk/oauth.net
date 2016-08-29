d1a9386805021a3b3f38deb83057f98bcc88ad53
Aaron Parecki <aaron@parecki.com>
2016-08-28 18:20:46 -0700

    more print css tweaks

--- books/index.php
+++ books/index.php
@@ -21,21 +21,29 @@ require('../includes/_header.php');
     <div id="main" class="column first last span-18 prepend-1 append-1">
       <h2>Books</h2>
     
-      <div style="height: 250px;">
-        <a href="https://www.manning.com/books/oauth-2-in-action"><img src="oauth_in_action.jpg" alt="OAuth in Action Cover" title="OAuth in Action" style="float:left; max-width: 25%; margin: 10px; border: 1px #ccc solid;"></a>
-        <p><a href="https://www.manning.com/books/oauth-2-in-action"><i>OAuth 2 in Action</i></a> by Justin Richer and Antonio Sanso covers the OAuth 2 protocol in depth, providing hands-on exercises for building clients, authorization servers, and protected resources in various configurations. The book covers the structure and components of an OAuth 2 system, common vunlerabilities and mitigations, and several protocols that are built on and around OAuth 2.</p>        
-        <p>This book is currently available for <a href="https://www.manning.com/books/oauth-2-in-action">early-access pre-order from the publisher</a>. Pre-ordering the book gives you access to portions of the book in electronic format as it's being completed by the authors. You'll also be able to provide feedback during the production process.</p>
+      <div class="book-row">
+        <div class="left">
+          <a href="https://www.manning.com/books/oauth-2-in-action" class="noprintexpand"><img src="oauth_in_action.jpg" alt="OAuth in Action Cover" title="OAuth in Action"></a>
+        </div>
+        <div class="right">
+          <p><a href="https://www.manning.com/books/oauth-2-in-action"><i>OAuth 2 in Action</i></a> by Justin Richer and Antonio Sanso covers the OAuth 2 protocol in depth, providing hands-on exercises for building clients, authorization servers, and protected resources in various configurations. The book covers the structure and components of an OAuth 2 system, common vunlerabilities and mitigations, and several protocols that are built on and around OAuth 2.</p>        
+          <p>This book is currently available for <a href="https://www.manning.com/books/oauth-2-in-action">early-access pre-order from the publisher</a>. Pre-ordering the book gives you access to portions of the book in electronic format as it's being completed by the authors. You'll also be able to provide feedback during the production process.</p>
+        </div>
       </div>
 	
-      <div style="">
+      <div>
         <?php require('../includes/_carbon.php'); ?>
       </div>
       <div style="clear:both; margin-bottom: 1em;"></div>
 
-      <div>
-        <a href="https://www.packtpub.com/application-development/mastering-oauth-2/?utm_source=GC-oauth.net&utm_medium=pod&utm_campaign=1784395407"><img src="mastering_oauth_2.jpg" alt="Mastering Oauth 2.0 Cover" title="Mastering OAuth 2.0" style="float:left; max-width: 25%; margin: 10px; border: 1px #ccc solid;"></a>
-        <p><a href="https://www.packtpub.com/application-development/mastering-oauth-2/?utm_source=GC-oauth.net&utm_medium=pod&utm_campaign=1784395407"><i>Mastering OAuth 2.0</i></a> by Charles Bihis provides an in-depth view of the OAuth 2.0 protocol from a client perspective.  With a focus on practicality and security, this book explores the various ways in which a client can integrate with an OAuth 2.0 service provider, discussing caveats and best practices along the way.<p>
-        <p>This book focuses on the client integration side of the OAuth 2.0 protocol and is ideal for client and application developers looking to integrate with OAuth 2.0 service providers in the most secure and effective way.</p>
+      <div class="book-row">
+        <div class="left">
+          <a href="https://www.packtpub.com/application-development/mastering-oauth-2/?utm_source=GC-oauth.net&utm_medium=pod&utm_campaign=1784395407" class="noprintexpand"><img src="mastering_oauth_2.jpg" alt="Mastering Oauth 2.0 Cover" title="Mastering OAuth 2.0"></a>
+        </div>
+        <div class="right">
+          <p><a href="https://www.packtpub.com/application-development/mastering-oauth-2/?utm_source=GC-oauth.net&utm_medium=pod&utm_campaign=1784395407"><i>Mastering OAuth 2.0</i></a> by Charles Bihis provides an in-depth view of the OAuth 2.0 protocol from a client perspective.  With a focus on practicality and security, this book explores the various ways in which a client can integrate with an OAuth 2.0 service provider, discussing caveats and best practices along the way.<p>
+          <p>This book focuses on the client integration side of the OAuth 2.0 protocol and is ideal for client and application developers looking to integrate with OAuth 2.0 service providers in the most secure and effective way.</p>
+        </div>
       </div>
     </div>
   </div>
