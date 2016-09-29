603672440d2e059c4f21872bf8ccad9e675afbb7
Aaron Parecki <aaron@parecki.com>
2016-09-29 08:21:40 -0700

    supported by okta

--- includes/_footer.php
+++ includes/_footer.php
@@ -14,7 +14,16 @@
     <?php if(time() > strtotime('2016-08-01') && time() < strtotime('2017-08-01')): ?>
       <div class="supported">
         <a href="http://auth0.com/">Supported by <img src="/images/auth0.svg" style="margin: 0 0.3em;"> Auth0</a>
+        <?php if(time() > strtotime('2016-10-01')): ?>
+          <span style="margin-left:4px;">and <img src="/images/okta.jpg" height="20" style="margin-bottom: 2px;"> <a href="http://okta.com">Okta</a></span>
+        <?php endif; ?>
       </div>
+    <?php else: ?>
+      <?php if(time() > strtotime('2016-10-01') && time() < strtotime('2017-09-30')): ?>
+        <div class="supported">
+          <a href="http://okta.com/">Supported by <img src="/images/okta.jpg" height="20" style="margin: 0 0.3em;"> Okta</a>
+        </div>
+      <?php endif; ?>      
     <?php endif; ?>
   </div>
 
