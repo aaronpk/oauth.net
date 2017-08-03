5c6480ecb7fc2fd558ac7c8659fdf26fc32cc5f5
Aaron Parecki <aaron@parecki.com>
2017-08-03 11:10:30 -0700

    footer

--- includes/_footer.php
+++ includes/_footer.php
@@ -11,7 +11,7 @@
       <a href="https://twitter.com/oauth_2" class="twitter-follow-button" data-show-count="false">Follow @oauth_2</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
     </div>
 
-    <?php if(time() > strtotime('2016-08-01') && time() < strtotime('2017-08-01')): ?>
+    <?php if(time() > strtotime('2016-08-01')): ?>
       <div class="supported">
         <a href="http://auth0.com/">Supported by <img src="/images/auth0.svg" style="margin: 0 0.3em;"> Auth0</a>
         <?php if(time() > strtotime('2016-10-01')): ?>
@@ -19,7 +19,7 @@
         <?php endif; ?>
       </div>
     <?php else: ?>
-      <?php if(time() > strtotime('2016-10-01') && time() < strtotime('2017-09-30')): ?>
+      <?php if(time() > strtotime('2016-10-01')): ?>
         <div class="supported">
           <a href="http://okta.com/">Supported by <img src="/images/okta.jpg" height="20" style="margin: 0 0.3em;"> Okta</a>
         </div>
