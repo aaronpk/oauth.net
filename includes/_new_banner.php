10e8cbb9333878b62082b160ad7b65f39c21e465
Aaron Parecki <aaron@parecki.com>
2018-04-23 08:17:02 -0700

    new post banner

--- includes/_new_banner.php
+++ includes/_new_banner.php
@@ -1,7 +1,7 @@
-<? if(time() < strtotime('2018-04-24')): ?>
+<? if(time() < strtotime('2018-04-19')): ?>
 <div class="alert alert-success" role="alert" style="border-radius: 0;">
   <div style="max-width:800px; margin: 0 auto; padding: 0 15px;">
-    New: <a href="https://developer.okta.com/blog/2018/04/10/oauth-authorization-code-grant-type" onclick="trackOutboundClick('https://developer.okta.com/blog/2018/04/10/oauth-authorization-code-grant-type'); return false;">What is the OAuth 2.0 Authorization Code Grant Type?</a>
+    New: <a href="" onclick="trackOutboundClick(''); return false;"></a>
   </div>
 </div>
 <? endif; ?>
