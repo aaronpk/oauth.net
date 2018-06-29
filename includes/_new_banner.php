1d4041b50fac2a82259080672787bb71a40e1613
Aaron Parecki <aaron@parecki.com>
2018-06-29 10:03:13 -0700

    new blog post

--- includes/_new_banner.php
+++ includes/_new_banner.php
@@ -1,7 +1,7 @@
-<? if(time() < strtotime('2018-06-18')): ?>
+<? if(time() < strtotime('2018-07-13')): ?>
 <div class="alert alert-success" role="alert" style="border-radius: 0;">
   <div style="max-width:800px; margin: 0 auto; padding: 0 15px;">
-    New: <a href="https://developer.okta.com/blog/2018/05/24/what-is-the-oauth2-implicit-grant-type" onclick="trackOutboundClick('https://developer.okta.com/blog/2018/05/24/what-is-the-oauth2-implicit-grant-type'); return false;">What is the OAuth 2.0 Implicit Grant Type?</a>
+    New: <a href="https://developer.okta.com/blog/2018/06/29/what-is-the-oauth2-password-grant-type" onclick="trackOutboundClick('https://developer.okta.com/blog/2018/06/29/what-is-the-oauth2-password-grant-type'); return false;">What is the OAuth 2.0 Implicit Grant Type?</a>
   </div>
 </div>
 <? endif; ?>
