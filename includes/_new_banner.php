195f745df51d547ecadea7355bb6c8de1cf99180
Aaron Parecki <aaron@parecki.com>
2018-07-16 11:40:25 -0700

    Update _new_banner.php

--- includes/_new_banner.php
+++ includes/_new_banner.php
@@ -1,8 +1,7 @@
-<? if(time() < strtotime('2018-07-13')): ?>
+<? if(time() < strtotime('2018-07-21')): ?>
 <div class="alert alert-success" role="alert" style="border-radius: 0;">
   <div style="max-width:800px; margin: 0 auto; padding: 0 15px;">
-    New: <a href="https://developer.okta.com/blog/2018/06/29/what-is-the-oauth2-password-grant" onclick="trackOutboundClick('https://developer.okta.com/blog/2018/06/29/what-is-the-oauth2-password-grant'); return false;">What is the OAuth 2.0 Password Grant Type?</a>
+    New: <a href="https://developer.okta.com/blog/2018/07/16/oauth-2-command-line" onclick="trackOutboundClick('https://developer.okta.com/blog/2018/07/16/oauth-2-command-line'); return false;">OAuth 2.0 from the Command Line</a>
   </div>
 </div>
 <? endif; ?>
-
