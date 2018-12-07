481535c697de9969cc59422b7f2715005a5d1c26
Aaron Parecki <aaron@parecki.com>
2018-12-07 13:36:29 -0800

    add new featured video

--- videos/index.php
+++ videos/index.php
@@ -12,12 +12,13 @@ require('../includes/_header.php');
       <h2>Videos</h2>
 
       <div style="margin-bottom: 1em">
-        <iframe width="100%" height="460" src="https://www.youtube.com/embed/wA4kqKFua2Q" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <iframe width="100%" height="460" src="https://www.youtube.com/embed/BNEoKexlmA4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
       </div>
 
       <h3>More Videos</h3>
 
       <ul>
+        <li><a href="https://www.youtube.com/watch?v=wA4kqKFua2Q">OAuth All the Things! What is OAuth 2.0?</a> by Aaron Parecki</li>
         <li><a href="https://www.youtube.com/watch?v=996OiexHze0">OAuth 2.0 and OpenID Connect (in plain English)</a> by Nate Barbettini</li>
         <li><a href="https://www.youtube.com/watch?v=rZRwE3d-gm0">Using OAuth and OpenID Connect in Your Applications - Oktane 18</a> by Aaron Parecki and Padma Govindarajalu</li>
       </ul>
