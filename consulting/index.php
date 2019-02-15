2ebe753d09af510a0d7b35a85758493241637b36
Aaron Parecki <aaron@parecki.com>
2019-02-14 19:05:22 -0800

    add bespoke engineering

--- consulting/index.php
+++ consulting/index.php
@@ -40,6 +40,19 @@ require('../includes/_header.php');
         </div>
       </div>
 
+      <div style="padding: 10px; border-radius: 8px; border: 1px #ccc solid; margin: 10px">
+        <h3><a href="https://bspk.io/">Bespoke Engineering</a></h3>
+
+        <div style="display: flex; flex-direction: row;">
+          <div style="background-color:#fff; padding: 1em; flex: 1 0;">
+            <img src="BSPK.png" width="100%" style="margin: 0;">
+          </div>
+          <div style="flex: 2 0; margin-left: 10px;">
+            <a href="https://bspk.io/">Bespoke Engineering</a> offers a variety of custom consulting services, including security architecture design, technical whitepapers and standards document editing.
+          </div>
+        </div>
+      </div>
+
       <p>Want to be listed here? <a href="https://github.com/aaronpk/oauth.net/blob/master/consulting/index.php">Send a pull request</a> with your information!</p>
 
     </div>
