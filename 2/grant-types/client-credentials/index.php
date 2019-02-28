fe9677e2554171b34e68a0d5a80e48a6f311c159
Aaron Parecki <aaron@parecki.com>
2019-02-27 16:12:34 -0800

    Update index.php

--- 2/grant-types/client-credentials/index.php
+++ 2/grant-types/client-credentials/index.php
@@ -18,7 +18,7 @@ require('../../../includes/_header.php');
 
     <h2>OAuth 2.0 Client Credentials Grant</h2>
 
-    <p><a href="https://tools.ietf.org/html/rfc6749#section-1.3.4" class="rfc">tools.ietf.org/html/rfc6749#section-1.3.4</a></p>
+    <p><a href="https://tools.ietf.org/html/rfc6749#section-4.4" class="rfc">tools.ietf.org/html/rfc6749#section-4.4</a></p>
 
     <p>The Client Credentials grant type is used by clients to obtain an access token outside of the context of a user.</p>
     <p>This is typically used by clients to access resources about themselves rather than to access a user's resources.</p>
