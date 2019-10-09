81d1eb236abaeb370f025ab6194f85c6f7b8831c
hackerman <3372410+aeneasr@users.noreply.github.com>
2019-10-09 15:14:27 +0200

    Update code/index.php

--- code/index.php
+++ code/index.php
@@ -68,7 +68,7 @@ require('./data.php');
     <span id="proxy-services-open-source"></span>
     <ul>
       <li><a href="https://github.com/oauth-io">OAuth.io</a></li>
-      <li><a href="https://github.com/ory/hydra">ORY Hydra</a></li>
+      <li><a href="https://www.ory.sh/hydra">ORY Hydra</a></li>
       <li><a href="https://github.com/ssqsignon">SSQ signon</a></li>
       <li><a href="https://github.com/babelouest/glewlwyd">Glewlwyd</a></li>
     </ul>
