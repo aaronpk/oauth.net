afe5c22f42856c168179b274a40ce2cecd89c147
Aaron Parecki <aaron@parecki.com>
2019-11-17 16:23:02 +0800

    add IETF106 agenda details

---
+++ events/2019-11-ietf106/index.php
@@ -0,0 +1,92 @@
+<?php
+$page_title = "OAuth Security Workshop &mdash; OAuth";
+$page_section = "workshop";
+$page_secondary = "";
+$page_meta_description = "";
+
+require('../../includes/_header.php');
+?>
+
+  <div class="container">
+
+    <div>
+      <h2>OAuth at IETF 106 Singapore</h2>
+
+      <p>November 16-22, 2019</p>
+
+      <h3 id="monday">TxAuth BoF - Monday</h3>
+      <p>13:30 - 15:30 • Padang room</p>
+      <ul>
+        <li>Introduction and Context - Chairs (10 min)</li>
+        <li>Limitations and Feature Requests
+          <ul>
+            <li>Limitations of OAuth - Justin (10 min)</li>
+            <li>Limitations of OAuth - Torsten (5 min)</li>
+            <li>Feature Requests - Torsten (5 min)</li>
+            <li>Feature Requests - Justin (10 min)</li>
+            <li>Non Authorize Features - Annabelle (15 min)</li>
+          </ul>
+        </li>
+        <li>Proposals
+          <ul>
+            <li>OAuth XYZ - Justin (20 min)
+              <ul><li><a href="https://oauth.xyz">oauth.xyz</a></li></ul>
+            </li>
+            <li>RAR & PAR - Torsten (20 min)
+              <ul>
+                <li><a href="https://tools.ietf.org/html/draft-lodderstedt-oauth-rar-03">oauth-rar-03</a></li>
+                <li><a href="https://tools.ietf.org/html/draft-lodderstedt-oauth-par-01">oauth-par-01</a></li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li>Discussion - Chairs (15 min)</li>
+        <li>Next Steps - Chairs (10 min)</li>
+      </ul>
+      <p class="small"><a href="https://datatracker.ietf.org/meeting/106/materials/agenda-106-txauth-00">official agenda</a></p>
+
+      <h3 id="wednesday">OAuth WG - Wednesday</h3>
+      <p>13:30 - 15:00 • Olivia room</p>
+      <ul>
+        <li>Chairs Update (15 min)</li>
+        <li>Security Topics – Torsten (15 min)
+          <ul><li><a href="https://tools.ietf.org/html/draft-ietf-oauth-security-topics-13">oauth-security-topics-13</a></li></ul>
+        </li>
+        <li>Browser-based Apps</a> – Aaron (30 min)
+          <ul><li><a href="https://tools.ietf.org/html/draft-ietf-oauth-browser-based-apps-04">oauth-browser-based-apps-04</a></li></ul>
+        </li>
+        <li>TXAuth update – Dick/Justin (15 min)
+          <ul><li><a href="https://oauth.xyz">oauth.xyz</a></li></ul>
+        </li>
+        <li>DPoP – Brian (15 min)
+          <ul><li><a href="https://tools.ietf.org/html/draft-fett-oauth-dpop-03">oauth-dpop-03</a></li></ul>
+        </li>
+      </ul>
+      <p class="small"><a href="https://datatracker.ietf.org/meeting/106/materials/agenda-106-oauth-01">official agenda</a></p>
+
+      <h3 id="thursday">OAuth WG - Thursday</h3>
+      <p>15:50 - 17:20 • Hullet room</p>
+      <ul>
+        <li>DPoP - Brian (10 min)
+          <ul><li><a href="https://tools.ietf.org/html/draft-fett-oauth-dpop-03">oauth-dpop-03</a></li></ul>
+        </li>
+        <li>Rich Authorization – Torsten (20 min)
+          <ul><li><a href="https://tools.ietf.org/html/draft-lodderstedt-oauth-rar-03">oauth-rar-03</a></li></ul>
+        </li>
+        <li>Pushed Authorization – Torsten (20 min)
+          <ul><li><a href="https://tools.ietf.org/html/draft-lodderstedt-oauth-par-01">oauth-par-01</a></li></ul>
+        </li>
+        <li>Client Intermediary Metadata – Aaron (20 min)
+          <ul><li><a href="https://tools.ietf.org/html/draft-parecki-oauth-client-intermediary-metadata-00">oauth-client-intermediary-metadata-00</a></li></ul>
+        </li>
+        <li>Authorization Framework: Claims – Travis (20 min)
+          <ul><li><a href="https://tools.ietf.org/html/draft-spencer-oauth-claims-00">oauth-claims-00</a></li></ul>
+        </li>
+      </ul>
+      <p class="small"><a href="https://datatracker.ietf.org/meeting/106/materials/agenda-106-oauth-01">official agenda</a></p>
+
+    </div>
+
+  </div>
+
+<?php require('../../includes/_footer.php'); ?>
