f3f24726b3d8a0c8f4df2d2c4271a37ae20e669c
Aaron Parecki <aaron@parecki.com>
2019-11-18 16:38:13 +0800

    add page on OAuth 3

---
+++ 3/index.php
@@ -0,0 +1,27 @@
+<?php
+$page_title = "OAuth 3";
+$page_section = "";
+$page_secondary = "";
+$page_meta_description = "OAuth 3 is an in-progress effort to redesign OAuth from the ground up";
+
+require('../includes/_header.php');
+?>
+
+<div class="container">
+  <div>
+
+    <h2 id="oauth-3">OAuth 3</h2>
+
+    <p><img src="/images/oauth-3-logo.png" alt="OAuth logo" width="140" style="float:right; margin: 0 0 8px 8px;">OAuth 3 is an in-progress effort to redesign OAuth from the ground up.</p>
+
+    <p>This specification and its extensions are being developed within the <a href="https://datatracker.ietf.org/wg/txauth/about/">IETF TXAuth Working Group</a>. Questions, suggestions and protocol changes should be discussed on the <a href="https://www.ietf.org/mailman/listinfo/txauth">TXAuth mailing list</a>.</p>
+
+    <p>Read the design details and exampels of OAuth 3 at <a href="https://oauth.xyz">oauth.xyz</a>.</p>
+
+    <p>This specification is very much in progress, and interested readers are encouraged to participate in its development by joining the IETF Working Group or attending <a href="/events/">OAuth events</a>.</p>
+
+
+  </div>
+</div>
+
+<?php require('../includes/_footer.php'); ?>
