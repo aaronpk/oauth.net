4769f90a3d76915320939d45e4a5f80fef5bd897
Aaron Parecki <aaron@parecki.com>
2020-01-21 08:29:50 -0800

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ require('./includes/_header.php');
     </div>
   </div>
   
-  <div>
+  <div style="margin-top: 10px;">
     <iframe width="100%" height="400" src="https://www.youtube.com/embed/KT8ybowdyr0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 
