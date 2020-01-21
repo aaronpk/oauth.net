df5fbbb5069b568377696d3f0bb28043591b5648
Aaron Parecki <aaron@parecki.com>
2020-01-21 08:33:42 -0800

    Update index.php

--- videos/index.php
+++ videos/index.php
@@ -18,6 +18,8 @@ require('../includes/_header.php');
       <h3>More Videos</h3>
 
       <ul>
+        <li><a href="https://www.youtube.com/watch?v=KT8ybowdyr0">What is OAuth and why does it matter? - OAuth in Five Minutes</a> - Aaron Parecki</li>
+        <li><a href="https://www.youtube.com/watch?v=PfvSD6MmEmQ">Securing Your APIs with OAuth 2.0 - API Days</a> - Aaron Parecki</li>
         <li><a href="https://www.youtube.com/watch?v=CHzERullHe8">What's going on with the Implicit flow?</a> - Aaron Parecki and Nate Barbettini</li>
         <li><a href="https://www.youtube.com/watch?v=H6MxsFMAoP8">OAuth: When Things Go Wrong</a> by Aaron Parecki</li>
         <li><a href="https://www.youtube.com/watch?v=BNEoKexlmA4">OAuth Access Tokens Explained</a> by Aaron Parecki</li>
