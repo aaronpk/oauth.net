6d34f586eb0c1e3b9d632edb7916cbd9e9b7ab9b
Aaron Parecki <aaron@parecki.com>
2020-07-07 16:51:10 -0700

    delete test file

--- includes/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-type: text/plain');
-
-echo 'REMOTE_ADDR: '.($_SERVER['REMOTE_ADDR'] ?? '')."\n";
-echo 'X_FORWARDED_FOR: '.($_SERVER['X_FORWARDED_FOR'] ?? '')."\n";
-
-print_r($_SERVER);
