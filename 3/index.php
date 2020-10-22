d54fc22eb42e996cc8de379c448670c6a134b474
Aaron Parecki <aaron@parecki.com>
2020-10-22 07:02:05 -0700

    add github link

--- 3/index.php
+++ 3/index.php
@@ -14,7 +14,7 @@ require('../includes/_header.php');
 
     <p><img src="/images/oauth-3-logo.png" alt="OAuth logo" width="140" style="float:right; margin: 0 0 8px 8px;">OAuth 3 is the unofficial name of GNAP, a new spec developed at the <a href="https://datatracker.ietf.org/doc/charter-ietf-gnap/">GNAP working group</a> at the IETF.</p>
 
-    <p>The new specification will encompass many more use cases than OAuth originally set out to solve. Questions, suggestions and protocol changes should be discussed on the <a href="https://www.ietf.org/mailman/listinfo/txauth">mailing list</a>.</p>
+    <p>The new specification will encompass many more use cases than OAuth originally set out to solve. Questions, suggestions and protocol changes should be discussed on the <a href="https://www.ietf.org/mailman/listinfo/txauth">mailing list</a> or <a href="https://github.com/ietf-wg-gnap/core-protocol">GitHub</a>.</p>
 
     <p>The latest version of the in-progress specification can be found at <a href="https://tools.ietf.org/html/draft-ietf-gnap-core-protocol">tools.ietf.org/html/draft-ietf-gnap-core-protocol</a>.</p>
 
