4d84b0bffe73e25db9355ca7f3903808e09491da
Aaron Parecki <aaron@parecki.com>
2020-10-22 08:54:58 -0700

    caps

--- 3/index.php
+++ 3/index.php
@@ -14,7 +14,7 @@ require('../includes/_header.php');
 
     <img src="/images/oauth-3-logo.png" alt="OAuth logo" width="140" style="float:right; margin: 0 0 8px 8px;">
 
-    <p>if you're looking for OAuth 3, there is no such protocol with that name.</p>
+    <p>If you're looking for OAuth 3, there is no such protocol with that name.</p>
 
     <p>However, efforts to create a next-generation protocol based on years of knowledge and experience with OAuth 2 are being held at the <a href="https://datatracker.ietf.org/doc/charter-ietf-gnap/">GNAP working group</a> at the IETF. The new specification will encompass many more use cases than OAuth originally set out to solve. Questions, suggestions and protocol changes should be discussed on the <a href="https://www.ietf.org/mailman/listinfo/txauth">mailing list</a> or <a href="https://github.com/ietf-wg-gnap/core-protocol">GitHub</a>.</p>
 
