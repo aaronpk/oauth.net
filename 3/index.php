2e5f41faae9f584b459dbe6293472157dfdef21c
Aaron Parecki <aaron@parecki.com>
2020-11-16 21:44:02 -0800

    move GNAP content to new GNAP page

--- 3/index.php
+++ 3/index.php
@@ -2,7 +2,7 @@
 $page_title = "OAuth 3";
 $page_section = "";
 $page_secondary = "";
-$page_meta_description = "OAuth 3 is an in-progress effort to redesign OAuth from the ground up";
+$page_meta_description = "OAuth 3 does not yet exist. You might be looking for OAuth 2.1 or GNAP";
 
 require('../includes/_header.php');
 ?>
@@ -14,26 +14,19 @@ require('../includes/_header.php');
 
     <img src="/images/oauth-3-logo.png" alt="OAuth logo" width="140" style="float:right; margin: 0 0 8px 8px;">
 
-    <p>If you're looking for OAuth 3, there is no such protocol with that name.</p>
+    <p>OAuth 3 does not exist. However, there are two independent efforts to continue to evolve the foundation that OAuth 2.0 has laid.</p>
 
-    <p>Efforts to create a next-generation protocol based on years of knowledge and experience with OAuth 2 are being held at the <a href="https://datatracker.ietf.org/doc/charter-ietf-gnap/">GNAP working group</a> at the IETF. When it reaches maturity and if there's IETF consensus to do so, the community may decide to adopt it as the next version of OAuth. The new specification will encompass many more use cases than OAuth originally set out to solve. Questions, suggestions and protocol changes should be discussed on the <a href="https://www.ietf.org/mailman/listinfo/txauth">mailing list</a> or <a href="https://github.com/ietf-wg-gnap/core-protocol">GitHub</a>.</p>
-
-    <p>The latest version of the in-progress specification can be found at <a href="https://tools.ietf.org/html/draft-ietf-gnap-core-protocol">tools.ietf.org/html/draft-ietf-gnap-core-protocol</a>.</p>
-
-    <p>Read the design details and examples that motivated the original design at <a href="https://oauth.xyz">oauth.xyz</a>. Early drafts of the spec were called TxAuth or Transactional Authorization.</p>
-
-    <p>This specification is very much in progress, and interested readers are encouraged to participate in its development by joining the IETF Working Group or attending <a href="https://events.oauth.net/">OAuth events</a>.</p>
-
-    <p>See Also: <b><a href="/2.1/">OAuth 2.1</a></b>, an officially adopted effort to consolidate and simplify the best practices of OAuth 2.0.</p>
+    <ul>
+      <li><b><a href="/2.1/">OAuth 2.1</a></b>, an officially adopted effort by the OAuth Working Group to consolidate and simplify the best practices of OAuth 2.0.</li>
+      <li><b><a href="/gnap/">GNAP</a></b>, a new IETF Working Group to develop a next-generation protocol that encompasses many use cases that are challenging to implement with OAuth 2.0.</li>
+    </ul>
 
     <p>More resources
       <ul>
       	<li><a href="https://medium.com/@justinsecurity/the-case-for-oauth-3-0-5c7537e3f9c3">The Case for OAuth 3.0</a> (Justin Richer)</li>
-        <li><a href="https://medium.com/@justinsecurity/xyz-interaction-a84091d2a8c8">XYZ: Interaction</a> (Justin Richer)</li>
         <li><a href="https://medium.com/@justinsecurity/xyz-compatibility-with-oauth-2-f7b490be536d">XYZ: Compatibility with OAuth 2.0</a> (Justin Richer)</li>
         <li><a href="https://aaronparecki.com/2019/07/18/17/adding-identity-to-xyz">Adding Identity to XYZ</a> (Aaron Parecki)</li>
       	<li><a href="https://medium.com/@justinsecurity/the-case-for-oauth-3-0-why-now-a15ffdc77fd5">The Case for OAuth 3.0: Why Now?</a> (Justin Richer)</li>
-        <li><a href="https://www.youtube.com/watch?v=U9i7YaN8v9c">Transactional Authorization - Identiverse 2019</a> (Justin Richer)</li>
       </ul>
     </p>
 
