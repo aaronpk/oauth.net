cfe34128cbf483b2fab09bb24e5a329d1cf7d223
Aaron Parecki <aaron@parecki.com>
2020-12-16 19:26:27 -0800

    replace video list with thumbnails from youtube playlist

--- videos/index.php
+++ videos/index.php
@@ -15,22 +15,26 @@ require('../includes/_header.php');
         <iframe width="100%" height="420" src="https://www.youtube.com/embed/g_aVPdwBTfw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
       </div>
 
-      <h3>More Videos</h3>
+      <div class="videos">
+      <?php
+      $playlist = json_decode(file_get_contents('videos.json'), true);
+      foreach($playlist['items'] as $video) {
+        echo '<div class="video">';
+          echo '<a href="https://www.youtube.com/watch?v='.$video['contentDetails']['videoId'].'"><img src="'.$video['snippet']['thumbnails']['medium']['url'].'"></a>';
+          echo '<div class="title"><a href="https://www.youtube.com/watch?v='.$video['contentDetails']['videoId'].'">'
+            .htmlspecialchars($video['snippet']['title'])
+            .'</a></div>';
+          echo '<div class="date">'
+            .date('F j, Y', strtotime($video['contentDetails']['videoPublishedAt']))
+            .'</div>';
+        echo '</div>';
+      }
+      ?>
+      </div>
 
+      <h3>Video Courses</h3>
       <ul>
-        <li><a href="https://www.udemy.com/course/oauth-2-simplified/?referralCode=B04F59AED67B8DA74FA7">The Nuts and Bolts of OAuth</a> (Video Course) - Aaron Parecki</li>
-        <li><a href="https://www.youtube.com/watch?v=aU9RsE4fcRM">How to Hack OAuth</a> - Aaron Parecki</li>
-        <li><a href="https://www.youtube.com/watch?v=espX8qKjywI">OAuth Phishing - OAuth in Five Minutes</a> - Aaron Parecki</li>
-        <li><a href="https://www.youtube.com/watch?v=5cQNwifDq1U">Confidential vs Public Clients - OAuth in Five Minutes</a> - Aaron Parecki</li>
-        <li><a href="https://www.youtube.com/watch?v=KT8ybowdyr0">What is OAuth and why does it matter? - OAuth in Five Minutes</a> - Aaron Parecki</li>
-        <li><a href="https://www.youtube.com/watch?v=PfvSD6MmEmQ">Securing Your APIs with OAuth 2.0 - API Days</a> - Aaron Parecki</li>
-        <li><a href="https://www.youtube.com/watch?v=CHzERullHe8">What's going on with the Implicit flow?</a> - Aaron Parecki and Nate Barbettini</li>
-        <li><a href="https://www.youtube.com/watch?v=H6MxsFMAoP8">OAuth: When Things Go Wrong</a> by Aaron Parecki</li>
-        <li><a href="https://www.youtube.com/watch?v=BNEoKexlmA4">OAuth Access Tokens Explained</a> by Aaron Parecki</li>
-        <li><a href="https://www.youtube.com/watch?v=wA4kqKFua2Q">OAuth All the Things! What is OAuth 2.0?</a> by Aaron Parecki</li>
-        <li><a href="https://www.youtube.com/watch?v=996OiexHze0">OAuth 2.0 and OpenID Connect (in plain English)</a> by Nate Barbettini</li>
-        <li><a href="https://www.youtube.com/watch?v=rZRwE3d-gm0">Using OAuth and OpenID Connect in Your Applications - Oktane 18</a> by Aaron Parecki and Padma Govindarajalu</li>
-        <li><a href="https://www.youtube.com/watch?v=vjCF_O6aZIg">Vulnerabilities of mobile OAuth 2.0</a> by Nikita Stupin</li>
+        <li><a href="https://www.udemy.com/course/oauth-2-simplified/?referralCode=B04F59AED67B8DA74FA7">The Nuts and Bolts of OAuth</a> - Aaron Parecki</li>
         <li><a href="https://classpert.com/oauth">Free and paid OAuth courses from popular e-learning providers (Classpert)</a></li>
       </ul>
 
