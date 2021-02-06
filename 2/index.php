a85450f119c1f19e8942daebb99e2f614ea9889d
Aaron Parecki <aaron@parecki.com>
2021-02-05 16:21:40 -0800

    link to video course

--- 2/index.php
+++ 2/index.php
@@ -17,13 +17,11 @@ require('../includes/_header.php');
     <!-- <p>OAuth 2.1 consolidates OAuth 2.0 and many common extensions under a new name.</p> -->
     <p>Questions, suggestions and protocol changes should be discussed on the <a href="https://www.ietf.org/mailman/listinfo/oauth">mailing list</a>.</p>
 
-    <?php if(time() < strtotime('2021-01-31')): ?>
     <div style="margin-top: 10px; margin-bottom: 20px; padding: 40px; border: 1px #ddd solid;">
       <div style="">New Video Course!</div>
       <div class="course-image"><a href="https://www.udemy.com/course/oauth-2-simplified/?referralCode=B04F59AED67B8DA74FA7" onclick="trackOutboundClick('https://www.udemy.com/course/oauth-2-simplified/?referralCode=B04F59AED67B8DA74FA7', '7VBGSQN1'); return false;"><img src="https://oauth2simplified.com/images/nuts-and-bolts-of-oauth.png" style="width: 100%"></a></div>
       <div style="text-align: right;">by Aaron Parecki</div>
     </div>
-    <?php endif ?>
 
 
     <h3 style="clear:none;">OAuth 2.0</h3>
