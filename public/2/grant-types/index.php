228b4db47d188a95d77a9ebb760de0739376dd14
Andy Piper <andypiper@users.noreply.github.com>
2021-06-07 17:58:17 +0100

    Add PKCE link to the Grant Types index
    This aligns the Grant Types index page with the top-level index page.

--- public/2/grant-types/index.php
+++ public/2/grant-types/index.php
@@ -23,6 +23,7 @@ require('../../../includes/_header.php');
 
     <ul>
       <li><a href="/2/grant-types/authorization-code/">Authorization Code</a></li>
+      <li><a href="/2/pkce/">PKCE</a></li>
       <li><a href="/2/grant-types/client-credentials/">Client Credentials</a></li>
       <li><a href="/2/grant-types/device-code/">Device Code</a></li>
       <li><a href="/2/grant-types/refresh-token/">Refresh Token</a></li>
