d04b477f7c6cc9e26d40ca4f070e40f2bed148ff
Daniel Fett <mail@danielfett.de>
2022-02-18 18:31:23 +0100

    Add OSW 2022 and new OSW website

--- public/workshop/index.php
+++ public/workshop/index.php
@@ -12,19 +12,20 @@ require('../../includes/_header.php');
     <div>
       <h2>OAuth Security Workshops</h2>
 
-      <p>The OAuth Security Workshop (OSW) aim is to improve the security of OAuth and related Internet protocols by a direct exchange of views between academic researchers, IETF OAuth Working Group members and industry.</p>
+      <p>The aim of the <a href="https://oauth.secworkshop.events/">OAuth Security Workshop (OSW)</a> is to improve the security of OAuth and related Internet protocols by a direct exchange of views between academic researchers, IETF OAuth Working Group members and industry.</p>
 
     </div>
     <div>
       <h3>Next Event</h3>
       <ul>
-        <li><a href="https://barcamps.eu/osw2021/">OSW2021, Online</a></li>
+        <li><a href="https://oauth.secworkshop.events/osw2022">OSW2022, Trondheim, Norway</a> (oauth.secworkshop.events)</li>
       </ul>
     </div>
     <div>
       <h3>Past Events</h3>
       <ul>
-        <li><a href="https://osw2020.com/">OSW2020, Online</a></li>
+        <li><a href="https://barcamps.eu/osw2021/">OSW2021, Online</a> (barcamps.eu)</li>
+        <li><a href="https://osw2020.com/">OSW2020, Online</a> (osw2020.com)</li>
         <li><a href="https://sec.uni-stuttgart.de/events/osw2019">OSW2019, Stuttgart, Germany</a> (sec.uni-stuttgart.de)</li>
         <li><a href="https://st.fbk.eu/osw2018">OSW2018, Trento, Italy</a> (st.fbk.eu)</li>
         <li><a href="https://zisc.ethz.ch/oauth-security-workshop-2017/">OSW2017, Zurich, Switzerland</a> (zisc.ethz.ch)</li>
