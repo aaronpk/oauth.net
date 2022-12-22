f17ce8332f06144b4effcac36384483046f69ca4
Aaron Parecki <aaron@parecki.com>
2022-12-22 08:56:02 -0800

    update FAPI page

--- public/fapi/index.php
+++ public/fapi/index.php
@@ -14,16 +14,18 @@ require('../../includes/_header.php');
 
     <p>FAPI 2.0 is an API security profile based on the OAuth 2.0 framework suitable for protecting APIs in high-value scenarios.</p>
 
-    <p>The FAPI work is split into two documents</p>
+    <p>The core FAPI functionality is split into two documents, with a third that describes the attacker model</p>
     <ul>
-      <li><a href="https://openid.bitbucket.io/fapi/fapi-2_0-security.html">FAPI 2.0 Security Profile</a></li>
+      <li><a href="https://openid.bitbucket.io/fapi/fapi-2_0-security-profile.html">FAPI 2.0 Security Profile</a></li>
       <li><a href="https://openid.bitbucket.io/fapi/fapi-2_0-message-signing.html">FAPI 2.0 Message Signing</a></li>
+      <li><a href="https://openid.bitbucket.io/fapi/fapi-2_0-attacker-model.html">FAPI 2.0 Attacker Model</a></li>
     </ul>
 
     <p>More resources
       <ul>
         <li><a href="https://identityunlocked.auth0.com/public/49/Identity%2C-Unlocked.--bed7fada/d847cfb7">Exploring Financial-Grade API (FAPI) with Torsten</a> (Identity Unlocked Podcast)</li>
         <li><a href="https://www.linkedin.com/pulse/fapi-20-security-profile-simpler-yes-wait-compulsory-standardised-">FAPI 2.0: Security Profile (Simpler Security, Yes!)</a> (Raidiam)</li>
+        <li><a href="https://openid.bitbucket.io/fapi/">All OpenID FAPI Working Group Drafts</a></li>
       </ul>
     </p>
 
