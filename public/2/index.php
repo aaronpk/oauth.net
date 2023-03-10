9a56d0c9039a06e6ccc28d6e4a5901e75b9b1bf5
Aaron Parecki <aaron@parecki.com>
2023-03-09 16:44:43 -0800

    add page on passkeys

--- public/2/index.php
+++ public/2/index.php
@@ -129,6 +129,7 @@ require('../../includes/_header.php');
     <ul>
       <li><a href="/fapi/">FAPI</a> (OpenID Foundation)</li>
       <li><a href="/webauthn/">WebAuthn - Web Authentication</a></li>
+      <li><a href="/passkeys">passkeys</a> are a new way to sign in to services without a password</li>
       <li><a href="/http-signatures/">Signing HTTP Messages</a> - A generic HTTP message signing spec</li>
       <li><a href="/openid-for-verifiable-credentials/">OpenID for Verifiable Credentials</a></li>
     </ul>
