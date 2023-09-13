f5e4905b1078bae989bd2165068d8a69f47ac8b7
Michiel <michielalbracht@me.com>
2023-09-13 18:33:14 +0200

    DPOP is RFC9449

--- public/2/dpop/index.php
+++ public/2/dpop/index.php
@@ -9,16 +9,16 @@ require('../../../includes/_header.php');
   <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
       <li class="breadcrumb-item"><a href="/2/">OAuth 2.0</a></li>
-      <li class="breadcrumb-item active">DPoP - Demonstrating Proof of Possession at the Application Layer</li>
+      <li class="breadcrumb-item active">DPoP - Demonstrating Proof of Possession</li>
     </ol>
   </nav>
   <div>
 
-    <h2>Draft: OAuth 2.0 Demonstrating Proof-of-Possession at the Application Layer (DPoP)</h2>
+    <h2>OAuth 2.0 Demonstrating Proof-of-Possession (DPoP)</h2>
 
-    <p><a href="https://tools.ietf.org/html/draft-ietf-oauth-dpop" class="rfc">tools.ietf.org/html/draft-ietf-oauth-dpop</a></p>
+    <p><a href="https://tools.ietf.org/html/rfc9449" class="rfc">tools.ietf.org/html/rfc9449</a></p>
 
-    <p><a href="https://tools.ietf.org/html/draft-ietf-oauth-dpop">DPoP</a>, or Demonstrating Proof of Possession, is an extension that describes a technique to cryptographically bind access tokens to a particular client when they are issued. This is one of many attempts at improving the security of <a href="/2/bearer-tokens/">Bearer Tokens</a> by requiring the application using the token to prove possession of the same private key that was used to obtain the token.</p>
+    <p><a href="https://tools.ietf.org/html/rfc9449">DPoP</a>, or Demonstrating Proof of Possession, is an extension that describes a technique to cryptographically bind access tokens to a particular client when they are issued. This is one of many attempts at improving the security of <a href="/2/bearer-tokens/">Bearer Tokens</a> by requiring the application using the token to prove possession of the same private key that was used to obtain the token.</p>
 
     <p>See Also:</p>
     <ul>
