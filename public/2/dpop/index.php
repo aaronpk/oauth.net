b3f8da47aeb893aa5a23ab8d9df054213215437d
Aaron Parecki <aaron@parecki.com>
2023-10-20 10:31:14 -0700

    DPoP -> RFC9449

--- public/2/dpop/index.php
+++ public/2/dpop/index.php
@@ -1,8 +1,8 @@
 <?php
-$page_title = "OAuth 2.0 DPoP - Demonstrating Proof of Possession at the Application Layer";
+$page_title = "OAuth 2.0 DPoP - Demonstrating Proof of Possession at the Application Layer- RFC9449";
 $page_section = "";
 $page_secondary = "";
-$page_meta_description = "OAuth 2.0 DPoP - Demonstrating Proof of Possession at the Application Layer";
+$page_meta_description = "OAuth 2.0 DPoP - Demonstrating Proof of Possession at the Application Layer - RFC9449";
 require('../../../includes/_header.php');
 ?>
 <div class="container">
@@ -14,11 +14,11 @@ require('../../../includes/_header.php');
   </nav>
   <div>
 
-    <h2>Draft: OAuth 2.0 Demonstrating Proof-of-Possession at the Application Layer (DPoP)</h2>
+    <h2>RFC9449: OAuth 2.0 Demonstrating Proof-of-Possession at the Application Layer (DPoP)</h2>
 
-    <p><a href="https://tools.ietf.org/html/draft-ietf-oauth-dpop" class="rfc">tools.ietf.org/html/draft-ietf-oauth-dpop</a></p>
+    <p><a href="https://datatracker.ietf.org/doc/html/rfc9449" class="rfc">datatracker.ietf.org/doc/html/rfc9449</a></p>
 
-    <p><a href="https://tools.ietf.org/html/draft-ietf-oauth-dpop">DPoP</a>, or Demonstrating Proof of Possession, is an extension that describes a technique to cryptographically bind access tokens to a particular client when they are issued. This is one of many attempts at improving the security of <a href="/2/bearer-tokens/">Bearer Tokens</a> by requiring the application using the token to prove possession of the same private key that was used to obtain the token.</p>
+    <p><a href="https://datatracker.ietf.org/doc/html/rfc9449">DPoP</a>, or Demonstrating Proof of Possession, is an extension that describes a technique to cryptographically bind access tokens to a particular client when they are issued. This is one of many attempts at improving the security of <a href="/2/bearer-tokens/">Bearer Tokens</a> by requiring the application using the token to prove possession of the same private key that was used to obtain the token.</p>
 
     <p>See Also:</p>
     <ul>
