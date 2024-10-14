b50234344b89ec5ff5beec67834847ec4630c07c
Aaron Parecki <aaron@parecki.com>
2024-10-14 13:36:14 -0700

    add links to related specs and working groups

--- public/ipsie/index.php
+++ public/ipsie/index.php
@@ -12,7 +12,9 @@ require('../../includes/_header.php');
 
     <h2>IPSIE - Interoperability Profile for Secure Identity in the Enterprise</h2>
 
-    <p>The Interoperability Profiling for Secure Identity in the Enterprise (IPSIE) Working Group develops interoperability and security profiles of existing specifications that enable secure identity management within the enterprise.</p>
+    <p>The Interoperability Profiling for Secure Identity in the Enterprise (IPSIE) Working Group develops interoperability and security profiles of existing specifications such as OpenID Connect that enable secure identity management within the enterprise.</p>
+
+    <p>The initial problem space the working group is focused on includes single sign-on, lifecycle management, entitlements, risk signal sharing, logout, and token revocation. These correspond with existing standards such as <a href="https://openid.net/wg/connect/">OpenID Connect</a>, <a href="https://scim.cloud">SCIM</a>, <a href="https://openid.net/wg/sharedsignals/">Shared Signals and Events</a>, and OAuth.</p>
 
     <ul>
       <li>
