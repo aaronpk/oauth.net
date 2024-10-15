7919d9a0022aebd518066cef41b5c7b15310a079
Aaron Parecki <aaron@parecki.com>
2024-10-15 12:34:59 -0700

    schedule links for tomorrow

--- public/ipsie/index.php
+++ public/ipsie/index.php
@@ -17,9 +17,11 @@ require('../../includes/_header.php');
     <p>The initial problem space the working group is focused on includes single sign-on, lifecycle management, entitlements, risk signal sharing, logout, and token revocation. These correspond with existing standards such as <a href="https://openid.net/wg/connect/">OpenID Connect</a>, <a href="https://scim.cloud">SCIM</a>, <a href="https://openid.net/wg/sharedsignals/">Shared Signals and Events</a>, and OAuth.</p>
 
     <ul>
-      <li>
-        <a href="https://openid.net/wg/ipsie/">IPSIE Working Group Home Page</a>
-      </li>
+      <li><a href="https://openid.net/wg/ipsie/">IPSIE Working Group Home Page</a> (openid.net)</li>
+      <?php if(time() > strtotime('2024-10-16T06:00:00-0700')): ?>
+        <li><a href="https://openid.net/announcing-ipsie-working-group/">Announcing the IPSIE Working Group</a> (openid.net)</li>
+        <li><a href="https://www.okta.com/blog/2024/10/oktas-mission-to-standardize-identity-security/">Okta's mission to standardize Identity Security</a> (okta.com)</li>
+      <?php endif ?>
     </ul>
 
     <!--
